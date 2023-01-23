736f7df119584402d44d1a3855cd5b1db2ba7b23
Hugos68 <hugokorteapple@gmail.com>
2023-01-23 13:47:41 +0100

    added subjective card

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,19 @@
 <h1 class="text-center">Projects</h1>
 <div class="w-full h-0.5 my-2 bg-surface-100-800-token"></div>
 <div class="grid mt-10 grid-cols-3 grid-flow-row gap-10">
-    <div class="card card-glass-primary p-4">
-        <p>Subjective</p>
-        <div>
-            <a href="https://github.com/Hugos68/Subjective-2">View Code</a>
-            <a href="https://subjective.vercel.app/home">Live Demo</a>
+    <div class="card w-full h-full aspect-video card-glass-primary">
+        <div class="relative translate-x-4 translate-y-4 aspect-video group">
+            <img class="absolute group-hover:brightness-50 transition-all" src="subjective.png" alt="subjective_front_page">
+            <div class="hidden group-hover:block transition-all">
+                <p>Subjective</p>
+                <div class="w-full flex justify-evenly">
+                    <a class="btn btn-ringed-secondary" href="https://github.com/Hugos68/Subjective-2">
+                        <span>&lt;/&gt;</span>
+                        <span>View Code</span>
+                    </a>
+                    <a class="btn btn-ringed-primary" href="https://subjective.vercel.app/home">Live Demo</a>
+                </div>
+            </div> 
         </div>
     </div>
     <div class="card card-glass-primary p-4">
