af6f884ee43f0e7b81a9be87ffe57ab79cf2b3c8
Hugos68 <hugokorteapple@gmail.com>
2023-01-23 15:42:40 +0100

    Added project description

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,6 +4,7 @@
     const projects = [   
         {
             title: "Subjective",
+            description: "Subjective is a webshop to buy clothes and such, not only have I built this project but am also a stakeholder in the company",
             codeLink : "https://github.com/Hugos68/Subjective-2",
             demoLink : "https://subjective.vercel.app/home",
             bgImageSrc : "subjective.png",
@@ -11,6 +12,7 @@
         },
         {
             title: "Blugo",
+            description: "Blugo is an online blog used to blog your daily activites, I've built it to practise user mangament and authentication.",
             codeLink : "https://github.com/Hugos68/blugo",
             demoLink : "https://blugo.vercel.app/home",
             bgImageSrc : "blugo.png",
@@ -18,8 +20,9 @@
         },
         {
             title: "Portfolio",
+            description: "My portfolio is a personal project to keep track of my skills and projects and to show what I am capable of.",
             codeLink : "https://github.com/Hugos68/portfolio",
-            demoLink : "https://portfolio.vercel.app/",
+            demoLink : "https://portfolio-hugos68.vercel.app/",
             bgImageSrc : "portfolio.png",
             bgImageAlt : "portfolio-landing-page"
         },
@@ -30,6 +33,7 @@
         <div in:fly={{delay: 50 * i, y: 50}}>
             <ProjectCard 
                 title={project.title}
+                description={project.description}
                 codeLink={project.codeLink}
                 demoLink={project.demoLink}
                 bgImageSrc={project.bgImageSrc}
