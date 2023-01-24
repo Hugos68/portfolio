84f53b651188ecceb204b9f0de34b3b99a36776f
hugos68 <hugokorteapple@gmail.com>
2023-01-25 00:56:46 +0100

    Updated skills, still need some form of interactivity

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -12,21 +12,27 @@
             <AccordionItem>
                 <svelte:fragment slot="lead">🖥️ <strong>Frontend</strong></svelte:fragment>
                 <svelte:fragment slot="summary">Web development and interacting with the end user.</svelte:fragment>
-                <svelte:fragment slot="content">(content)</svelte:fragment>
+                <svelte:fragment slot="content">
+                    HTML, CSS, JavaScript, TypeScript, Svelte, Tailwind
+                </svelte:fragment>
             </AccordionItem>
         </div>
         <div in:fly={{delay: 500, duration: 1000, y: 250, easing: cubicOut}}>
             <AccordionItem>
                 <svelte:fragment slot="lead">⚙️ <strong>Backend</strong></svelte:fragment>
                 <svelte:fragment slot="summary">Business logic and Databases.</svelte:fragment>
-                <svelte:fragment slot="content">(content)</svelte:fragment>
+                <svelte:fragment slot="content">
+                    Java, C#, PostgresSQL, MySQL, Node, AL, Business Central
+                </svelte:fragment>
             </AccordionItem>
         </div>
         <div in:fly={{delay: 750, duration: 1000, y: 250, easing: cubicOut}}>
             <AccordionItem>
                 <svelte:fragment slot="lead">🔧 <strong>Tooling</strong></svelte:fragment>
                 <svelte:fragment slot="summary">Tools/Concepts I am comfortable and familiar with.</svelte:fragment>
-                <svelte:fragment slot="content">(content)</svelte:fragment>
+                <svelte:fragment slot="content">
+                    RestAPI, Git, Docker, Figma, Photoshop
+                </svelte:fragment>
             </AccordionItem>
         </div>
 </AccordionGroup>
