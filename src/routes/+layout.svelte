484cd48e1df9248d378878c0e81b22bc78d74bc3
Hugos68 <hugokorteapple@gmail.com>
2023-01-26 10:20:36 +0100

    Added better styling and responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 	import '../app.postcss';
 
 	import { AppShell, Drawer, LightSwitch, Modal, Toast } from '@skeletonlabs/skeleton';
-	import SlotWrapper from '$lib/components/SlotWrapper.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 </script>
@@ -18,7 +17,7 @@
 <!-- MAIN STRUCTURE -->
 <AppShell>
 	<svelte:fragment slot="header"><Header /></svelte:fragment>
-	<SlotWrapper><slot /></SlotWrapper>
+	<slot />
 	<svelte:fragment slot="pageFooter"><Footer /></svelte:fragment>
 </AppShell>
 
--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -8,8 +8,7 @@
 <AccordionGroup spacing='space-y-12' padding="p-8">
         <div in:fly={{duration: 750, y: 250, easing: cubicOut}}>
             <AccordionItem>
-                <svelte:fragment slot="lead">🖥️ <strong>Frontend</strong></svelte:fragment>
-                <svelte:fragment slot="summary">Web development and interacting with the end user.</svelte:fragment>
+                <svelte:fragment slot="summary">🖥️ <strong>Frontend</strong></svelte:fragment>
                 <svelte:fragment slot="content">
                     HTML, CSS, JavaScript, TypeScript, Svelte, Tailwind
                 </svelte:fragment>
@@ -17,8 +16,7 @@
         </div>
         <div in:fly={{delay: 250, duration: 750, y: 250, easing: cubicOut}}>
             <AccordionItem>
-                <svelte:fragment slot="lead">⚙️ <strong>Backend</strong></svelte:fragment>
-                <svelte:fragment slot="summary">Business logic and Databases.</svelte:fragment>
+                <svelte:fragment slot="summary">⚙️ <strong>Backend</strong></svelte:fragment>
                 <svelte:fragment slot="content">
                     Java, C#, PostgresSQL, MySQL, Node, AL, Business Central
                 </svelte:fragment>
@@ -26,8 +24,7 @@
         </div>
         <div in:fly={{delay: 500, duration: 750, y: 250, easing: cubicOut}}>
             <AccordionItem>
-                <svelte:fragment slot="lead">🔧 <strong>Tooling</strong></svelte:fragment>
-                <svelte:fragment slot="summary">Tools/Concepts I am comfortable and familiar with.</svelte:fragment>
+                <svelte:fragment slot="summary">🔧 <strong>Tooling</strong></svelte:fragment>
                 <svelte:fragment slot="content">
                     RestAPI, Git, Docker, Figma, Photoshop
                 </svelte:fragment>
