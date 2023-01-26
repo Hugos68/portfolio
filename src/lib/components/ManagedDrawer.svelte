d9d47c140becf18f50b12eba8366f90a44bed810
Hugos68 <hugokorteapple@gmail.com>
2023-01-26 10:55:49 +0100

    Readded gaps for nav

--- src/lib/components/ManagedDrawer.svelte
+++ src/lib/components/ManagedDrawer.svelte
@@ -7,7 +7,7 @@
 <Drawer>
 	{#if $drawerStore.id === 'hamburger'}
         <nav>
-            <ul class="list-nav flex items-center">
+            <ul class="list-nav flex gap-2 items-center">
                 <li><a href="/about" class:bg-opacity-10={$page.route.id==="/about"} class:bg-primary-500={$page.route.id==="/about"}>About Me</a></li>
                 <li><a href="/skills" class:bg-opacity-10={$page.route.id==="/skills"} class:bg-primary-500={$page.route.id==="/skills"}>Skills</a></li>
                 <li><a href="/projects" class:bg-opacity-10={$page.route.id==="/projects"} class:bg-primary-500={$page.route.id==="/projects"}>Projects</a></li>
