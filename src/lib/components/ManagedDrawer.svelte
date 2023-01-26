47f9367e9508a3909872299e2735ea5be8697df7
Hugos68 <hugokorteapple@gmail.com>
2023-01-26 10:36:50 +0100

    Added clever hamburger menu

---
+++ src/lib/components/ManagedDrawer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { Drawer, drawerStore } from "@skeletonlabs/skeleton";
+</script>
+
+
+<Drawer>
+	{#if $drawerStore.id === 'hamburger'}
+        <nav>
+            <ul class="list-nav flex gap-2 items-center">
+                <li><a href="/about" class:bg-opacity-10={$page.route.id==="/about"} class:bg-primary-500={$page.route.id==="/about"}>About Me</a></li>
+                <li><a href="/skills" class:bg-opacity-10={$page.route.id==="/skills"} class:bg-primary-500={$page.route.id==="/skills"}>Skills</a></li>
+                <li><a href="/projects" class:bg-opacity-10={$page.route.id==="/projects"} class:bg-primary-500={$page.route.id==="/projects"}>Projects</a></li>
+            </ul>
+        </nav>
+    {/if}
+</Drawer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,23 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 
-	import { AppShell, Drawer, LightSwitch, Modal, Toast } from '@skeletonlabs/skeleton';
+	import { AppShell, LightSwitch, Modal, Toast } from '@skeletonlabs/skeleton';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import ManagedDrawer from '$lib/components/ManagedDrawer.svelte';
+	import SlotWrapper from '$lib/components/SlotWrapper.svelte';
 </script>
 
 <!-- OVERLAYS -->
 <Toast />
-<Drawer />
+<ManagedDrawer />
 <Modal padding="p-8"/>
 
 
 <!-- MAIN STRUCTURE -->
 <AppShell>
 	<svelte:fragment slot="header"><Header /></svelte:fragment>
-	<slot />
+	<SlotWrapper><slot /></SlotWrapper>
 	<svelte:fragment slot="pageFooter"><Footer /></svelte:fragment>
 </AppShell>
 
