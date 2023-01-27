fb72159bdfc5e6879296b698d491c7445da31734
Hugos68 <hugokorteapple@gmail.com>
2023-01-27 14:10:21 +0100

    Added dot and better timings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
         },
         {
             verb: "drinking",
-            subject: "coffee ;)"
+            subject: "coffee"
         },
         {
             verb: "writing",
@@ -84,10 +84,10 @@
         <p class="text-center" in:fly={{delay: 1000, duration: 1000, y: 100, easing: cubicOut}}>I am a</p>
         <h1 class="unstyled text-center selection:whitespace-nowrap text-[clamp(2rem,8vw,10rem)]" in:fly={{delay: 1500, duration: 1000, x: -100, easing: cubicOut}}>Software Developer.</h1>
     </div>
-    <p class="whitespace-nowrap" in:fly={{delay: 2250, duration: 500, easing: cubicOut}}>
+    <p class="whitespace-nowrap" in:fly={{delay: 2000, duration: 1000, easing: cubicOut}}>
         I love
         {#key currentComboIndex}
-            <strong in:typewriter={{delay: 750, speed: 0.75}} out:typewriter|local={{speed: 2.5}}>{verbSubjectCombo[currentComboIndex].verb + " " +verbSubjectCombo[currentComboIndex].subject}</strong>
+            <strong in:typewriter|local={{delay: 750, speed: 0.75}} out:typewriter|local={{speed: 2.5}}>{verbSubjectCombo[currentComboIndex].verb + " " +verbSubjectCombo[currentComboIndex].subject + "."}</strong>
         {/key}
     </p>
     <div class="w-min flex gap-[7.5vw]" in:fly={{delay: 2000, duration: 1000, y: 100, easing: cubicOut}}>
