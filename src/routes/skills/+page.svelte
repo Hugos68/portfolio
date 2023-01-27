238c11e9a3deac56b01b93219727a9cb9f84fc85
Hugos68 <hugokorteapple@gmail.com>
2023-01-27 08:11:33 +0100

    Added responsivenesse to the devicons

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,51 +1,62 @@
 <script lang="ts">
-	import { AccordionGroup, AccordionItem } from "@skeletonlabs/skeleton";
+	import { AccordionGroup, AccordionItem, tooltip } from "@skeletonlabs/skeleton";
 	import { fly } from "svelte/transition";
-    import { cubicOut } from "svelte/easing"
+    import { cubicOut, elasticOut } from "svelte/easing"
+
+    let frontendOpened: boolean = false;
+    let backendOpened: boolean = false;
+    let toolingOpened: boolean = false;
+
 </script>
 
 
-<AccordionGroup spacing='space-y-12' padding="p-8">
+<AccordionGroup spacing='space-y-12' padding="p-8" >
         <div in:fly={{duration: 750, y: 250, easing: cubicOut}}>
-            <AccordionItem>
+            <AccordionItem bind:open={frontendOpened}>
                 <svelte:fragment slot="summary">🖥️ <strong>Frontend</strong></svelte:fragment>
                 <svelte:fragment slot="content">
-                    <div class="w-full flex justify-evenly items-center">
-                        <i class="devicon-html5-plain colored skills-devicon"></i>
-                        <i class="devicon-css3-plain colored skills-devicon"></i>
-                        <i class="devicon-tailwindcss-plain colored skills-devicon"></i>
-                        <i class="devicon-javascript-plain colored skills-devicon"></i>
-                        <i class="devicon-typescript-plain colored skills-devicon"></i>
-                        <i class="devicon-svelte-plain colored skills-devicon"></i>    
-                    </div>
+                    {#if frontendOpened}
+                        <div class="w-full flex justify-evenly items-center" in:fly={{y: -100, duration: 250, easing: cubicOut}}>
+                            <i class="devicon-html5-plain colored skills-devicon" use:tooltip={{content: "HTML"}}></i>
+                            <i class="devicon-css3-plain colored skills-devicon" use:tooltip={{content: "CSS"}}></i>
+                            <i class="devicon-javascript-plain colored skills-devicon" use:tooltip={{content: "JavaScript"}}></i>
+                            <i class="devicon-typescript-plain colored skills-devicon" use:tooltip={{content: "TypeScript"}}></i>
+                            <i class="devicon-tailwindcss-plain colored skills-devicon" use:tooltip={{content: "Tailwind"}}></i>
+                            <i class="devicon-svelte-plain colored skills-devicon" use:tooltip={{content: "Svelte"}}></i>    
+                        </div>
+                    {/if}
                 </svelte:fragment>
             </AccordionItem>
         </div>
         <div in:fly={{delay: 250, duration: 750, y: 250, easing: cubicOut}}>
-            <AccordionItem>
+            <AccordionItem bind:open={backendOpened}>
                 <svelte:fragment slot="summary">⚙️ <strong>Backend</strong></svelte:fragment>
                 <svelte:fragment slot="content">
-                    <div class="w-full flex justify-evenly items-center">
-                        <i class="devicon-nodejs-plain colored skills-devicon"></i>
-                        <i class="devicon-java-plain colored skills-devicon"></i>
-                        <i class="devicon-csharp-plain colored skills-devicon"></i>
-                        <i class="devicon-postgresql-plain colored skills-devicon"></i>
-                        <i class="devicon-microsoftsqlserver-plain colored skills-devicon"></i>
-                    </div>
+                    {#if backendOpened}
+                        <div class="w-full flex justify-evenly items-center" in:fly={{y: -100, duration: 250, easing: cubicOut}}>
+                            <i class="devicon-nodejs-plain colored skills-devicon" use:tooltip={{content: "Node"}}></i>
+                            <i class="devicon-java-plain colored skills-devicon" use:tooltip={{content: "Java"}}></i>
+                            <i class="devicon-csharp-plain colored skills-devicon" use:tooltip={{content: "C#"}}></i>
+                            <i class="devicon-postgresql-plain colored skills-devicon" use:tooltip={{content: "PostgreSQL"}}></i>
+                            <i class="devicon-microsoftsqlserver-plain colored skills-devicon" use:tooltip={{content: "MicrosoftSQL"}}></i>
+                        </div>
+                    {/if}
                 </svelte:fragment>
             </AccordionItem>
         </div>
         <div in:fly={{delay: 500, duration: 750, y: 250, easing: cubicOut}}>
-            <AccordionItem>
+            <AccordionItem bind:open={toolingOpened}>
                 <svelte:fragment slot="summary">🔧 <strong>Tooling</strong></svelte:fragment>
                 <svelte:fragment slot="content">
-                    <div class="w-full flex justify-evenly items-center">
-                        <i class="devicon-rest-plain colored skills-devicon"></i>
-                        <i class="devicon-git-plain colored skills-devicon"></i>
-                        <i class="devicon-docker-plain colored skills-devicon"></i>
-                        <i class="devicon-figma-plain colored skills-devicon"></i>
-                        <i class="devicon-photoshop-plain colored skills-devicon"></i>
-                    </div>
+                    {#if toolingOpened}
+                        <div class="w-full flex justify-evenly items-center" in:fly={{y: -100, duration: 250, easing: cubicOut}}>
+                            <i class="devicon-rest-plain colored skills-devicon"></i>
+                            <i class="devicon-git-plain colored skills-devicon"></i>
+                            <i class="devicon-docker-plain colored skills-devicon"></i>
+                            <i class="devicon-figma-plain colored skills-devicon"></i>
+                            <i class="devicon-photoshop-plain colored skills-devicon"></i>
+                        </div>
+                    {/if}
                 </svelte:fragment>
             </AccordionItem>
         </div>
