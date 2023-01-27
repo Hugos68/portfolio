f507ec528f6b674fa394bc8e3f441dab935d120f
Hugos68 <hugokorteapple@gmail.com>
2023-01-27 14:01:10 +0100

    Added typewriter effect with some personality traits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,16 +15,85 @@
         };
         modalStore.trigger(modalSettings);
     }
+
+    interface VerbSubjectCombo {
+        verb: string,
+        subject: string
+    }
+    const verbSubjectCombo: VerbSubjectCombo[] = [
+        {
+            verb: "solving",
+            subject: "problems"
+        },
+        {
+            verb: "drinking",
+            subject: "coffee ;)"
+        },
+        {
+            verb: "writing",
+            subject: "code"
+        },
+        {
+            verb: "learning",
+            subject: "new things"
+        },
+        {
+            verb: "developing",
+            subject: "software"
+        },
+        {
+            verb: "playing",
+            subject: "games"
+        },
+    ];
+
+    let currentComboIndex: number = 0;
+    setInterval(() => {
+        if (currentComboIndex >= verbSubjectCombo.length-1) currentComboIndex = 0;
+        else currentComboIndex+=1;
+    }, 6000)
+
+    function typewriter(node, { delay = 0, speed = 1 }) {
+        const valid = (
+            node.childNodes.length === 1 &&
+            node.childNodes[0].nodeType === Node.TEXT_NODE
+        );
+
+        if (!valid) {
+            throw new Error(`This transition only works on elements with a single text node child`);
+        }
+
+        const text = node.textContent;
+        const duration = text.length / (speed * 0.01);
+
+        return {
+            delay,
+            duration,
+            tick: t => {
+                const i = Math.trunc(text.length * t);
+                node.textContent = text.slice(0, i);
+            }
+        };
+    }
 </script>
 
-
-<p class="mt-[5vh] text-center" in:fly={{duration: 1000, y: -100, easing: cubicOut}}>Hi my name is</p>
-<h1 class="unstyled text-center text-[clamp(2.75rem,11.5vw,10rem)] leading-normal" in:fly={{delay: 500, duration: 1000, x: 100, easing: cubicOut}}>Hugo Korte</h1>
-<p class="text-center"  in:fly={{delay: 1000, duration: 1000, y: 100, easing: cubicOut}}>I am a</p>
-<h1 class="unstyled text-center whitespace-nowrap text-[clamp(2rem,8.5vw,10rem)] leading-normal" in:fly={{delay: 1500, duration: 1000, x: -100, easing: cubicOut}}>Software Developer.</h1>
-<div class="w-min mx-auto flex gap-12 mt-[7.5vh]" in:fly={{delay: 2000, duration: 1000, y: 100, easing: cubicOut}}>
-    <button class="btn btn-lg variant-filled" on:click={() => openContactModal()}>Lets Talk!</button>
-    <a class="btn btn-lg variant-filled" href="Hugo_Korte_Resume_2023-25-1.pdf" download="Hugo_Korte_Resume_2023-25-1">Resume</a>
+<div class="h-full flex flex-col items-center justify-evenly">
+    <div>
+        <p class="text-center" in:fly={{duration: 1000, y: -100, easing: cubicOut}}>Hi my name is</p>
+        <h1 class="unstyled text-center text-[clamp(2.75rem,14vw,10rem)]" in:fly={{delay: 500, duration: 1000, x: 100, easing: cubicOut}}>Hugo Korte</h1>
+        <p class="text-center" in:fly={{delay: 1000, duration: 1000, y: 100, easing: cubicOut}}>I am a</p>
+        <h1 class="unstyled text-center selection:whitespace-nowrap text-[clamp(2rem,8vw,10rem)]" in:fly={{delay: 1500, duration: 1000, x: -100, easing: cubicOut}}>Software Developer.</h1>
+    </div>
+    <p class="whitespace-nowrap" in:fly={{delay: 2250, duration: 500, easing: cubicOut}}>
+        I love
+        {#key currentComboIndex}
+            <strong in:typewriter={{delay: 750, speed: 0.75}} out:typewriter|local={{speed: 2.5}}>{verbSubjectCombo[currentComboIndex].verb + " " +verbSubjectCombo[currentComboIndex].subject}</strong>
+        {/key}
+    </p>
+    <div class="w-min flex gap-[7.5vw]" in:fly={{delay: 2000, duration: 1000, y: 100, easing: cubicOut}}>
+        <button class="btn btn-lg variant-filled" on:click={() => openContactModal()}>Lets Talk!</button>
+        <a class="btn btn-lg variant-filled" href="Hugo_Korte_Resume_2023-25-1.pdf" download="Hugo_Korte_Resume_2023-25-1">Resume</a>
+    </div>
 </div>
 
 
