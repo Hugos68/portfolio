e7bac4872fd620697d91d7b498ceb7541b883a3b
Hugos68 <hugokorteapple@gmail.com>
2023-01-31 23:47:15 +0100

    Cleanup

--- src/lib/components/ProjectModal.svelte
+++ src/lib/components/ProjectModal.svelte
@@ -16,10 +16,10 @@
             <img src="{$modalStore[0].meta?.bgImageSrc}" class="bg-black/50 w-full aspect-video" alt="{$modalStore[0].meta?.bgImageAlt}" />
         </header>
         <div class="p-4 space-y-4">
-            <div class="flex gap-2">
+            <div class="flex flex-wr gap-2">
                 {#each $modalStore[0].meta?.techs as tech} 
-                <span class="chip variant-ghost-primary">{tech}</span>
-            {/each}
+                    <span class="chip variant-ghost-primary">{tech}</span>
+                {/each}
             </div>
     
             <h3><strong>{$modalStore[0].meta?.title}</strong></h3>
@@ -28,14 +28,15 @@
             </article>
         </div>
         <hr />
-        <footer class="p-4 flex justify-start items-center space-x-4">
+        <footer class="p-4 flex justify-start items-center gap-4">
             <div class="flex-auto flex justify-between items-center">
-                <a class="btn btn-sm lg:btn-lg variant-ghost-secondary p-3" href="{$modalStore[0].meta?.codeLink}" on:click|preventDefault={() => openLinkInNewTab($modalStore[0].meta?.codeLink)} >
+                <a class="btn lg:btn-lg variant-ghost-secondary p-3" href="{$modalStore[0].meta?.codeLink}" on:click|preventDefault={() => openLinkInNewTab($modalStore[0].meta?.codeLink)} >
                     <svg class="w-5 aspect-square" viewBox="0 0 512 512" fill="currentColor"><g><path class="st0" d="M256.005,0C114.611,0,0,114.611,0,255.996C0,397.39,114.611,512,256.005,512		C397.39,512,512,397.39,512,255.996C512,114.611,397.39,0,256.005,0z M187.066,203.112c0,0.366-0.157,0.689-0.436,0.916		l-72.053,53.582l72.053,53.608c0.279,0.192,0.436,0.541,0.436,0.89v33.07c0,0.41-0.201,0.776-0.532,0.968		c-0.148,0.079-0.296,0.13-0.471,0.13c-0.192,0-0.392-0.07-0.558-0.2L81.734,268.894c-0.27-0.201-0.445-0.541-0.445-0.898v-20.765		c0-0.366,0.174-0.706,0.445-0.907l103.771-77.18c0.166-0.131,0.366-0.192,0.558-0.192c0.174,0,0.323,0.044,0.471,0.13		c0.332,0.183,0.532,0.55,0.532,0.968V203.112z M238.519,360.404c-0.148,0.445-0.514,0.741-0.951,0.741h-29.006		c-0.331,0-0.628-0.175-0.82-0.472c-0.183-0.278-0.236-0.645-0.131-0.976l65.87-208.118c0.131-0.437,0.514-0.724,0.942-0.724h29.014		c0.314,0,0.628,0.166,0.811,0.454c0.183,0.288,0.236,0.654,0.131,0.994L238.519,360.404z M430.694,267.996		c0,0.357-0.156,0.698-0.436,0.898l-103.771,77.181c-0.166,0.13-0.357,0.2-0.558,0.2c-0.166,0-0.322-0.052-0.471-0.13		c-0.331-0.192-0.532-0.558-0.532-0.968v-33.062c0-0.357,0.157-0.706,0.436-0.898l72.053-53.608l-72.053-53.582		c-0.278-0.209-0.436-0.54-0.436-0.906V170.05c0-0.418,0.201-0.785,0.532-0.968c0.148-0.086,0.305-0.13,0.471-0.13		c0.201,0,0.392,0.061,0.558,0.192l103.771,77.18c0.28,0.201,0.436,0.541,0.436,0.907V267.996z"/></g></svg>
                     <span>View Code</span>
                 </a>
-                <a class="btn btn-sm lg:btn-lg variant-ghost-primary p-3" href="{$modalStore[0].meta?.demoLink}" on:click|preventDefault={() => openLinkInNewTab($modalStore[0].meta?.demoLink)}>
-                    <svg class="w-5 aspect-square" viewBox="0 0 24 24"  fill="currentColor"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M13 3H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2v-3" />  <path d="M8 21h8" />  <path d="M12 17v4" />  <path d="M17 8l5-5" />  <path d="M17 3h5v5" /></svg>                    <span>Live Demo</span>
+                <a class="btn lg:btn-lg variant-ghost-primary p-3" href="{$modalStore[0].meta?.demoLink}" on:click|preventDefault={() => openLinkInNewTab($modalStore[0].meta?.demoLink)}>
+                    <svg class="w-5 aspect-square" viewBox="0 0 24 24"  fill="currentColor"  stroke="#000000"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M13 3H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2v-3" />  <path d="M8 21h8" />  <path d="M12 17v4" />  <path d="M17 8l5-5" />  <path d="M17 3h5v5" /></svg>
+                    <span>Live Demo</span>
                 </a>
             </div>
         </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,12 +17,12 @@
     }
 
     const personalitySentences: string[] = [
-        "solving problems.",
-        "drinking coffee.",
-        "writing code.",
-        "learning new things.",
-        "developing software.",
-        "thinking outside the box."
+        "solving problems 🧩.",
+        "drinking coffee ☕.",
+        "writing code 💻.",
+        "learning new things 📒.",
+        "developing software 🌐.",
+        "thinking outside the box 💡."
     ];
 
     let sentenceIndex: number = 0;
@@ -32,11 +32,8 @@
             node.childNodes[0].nodeType === Node.TEXT_NODE
         );
 
-        if (!valid) {
-            console.log(node.childNodes.length);
-            console.log(node.childNodes[0].nodeType);
-            throw new Error(`This transition only works on elements with a single text node child`);
-        }
+        if (!valid) throw new Error(`This transition only works on elements with a single text node child`);
+    
         const text: string = node.textContent || "";
         const duration = text.length / (speed * 0.01);
 
@@ -53,9 +50,8 @@
 </script>
 
 <div class="h-full flex flex-col items-center justify-evenly">
-    <div class="relative p-8">  
-        <div class="absolute w-full h-full border-4 border-current top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] skew-x-[-2.5deg] skew-y-[-5deg]" in:fade={{delay: 250, duration: 1000, easing: cubicOut}}></div>
-
+    <div class="relative">  
+        <div class="animate-skew absolute w-full h-full border-2 border-current top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] skew-x-[-5deg] skew-y-[-5deg] scale-[135%]" in:fade={{delay: 250, duration: 1000, easing: cubicOut}}></div>
         <p class="unstyled text-[clamp(2.75rem,10vw,5rem)]" in:fly={{duration: 1000, x: 100, easing: cubicOut}}>Hugo Korte</p>
         <p class="unstyled text-[clamp(1rem,5vw,3rem)]" in:fly={{delay: 100, duration: 1000, x: 100, easing: cubicOut}}>
             I am a Software Developer.
@@ -65,7 +61,7 @@
         I love&nbsp;
         {#key sentenceIndex}
             <strong
-                in:typewriter={{delay: 750, speed: 0.75}}
+                in:typewriter={{delay: 825, speed: 0.75}}
                 out:typewriter|local={{speed: 2.5}}
                 on:outrostart="{() => blinkingCursor.classList.remove('blink')}"
 
@@ -78,8 +74,8 @@
         <span class="block" bind:this={blinkingCursor}>|</span>
     </p>
     <div class="w-min flex gap-[7.5vw]" in:fly={{delay: 300, duration: 1000, y: 100, easing: cubicOut}}>
-        <button class="btn btn-lg variant-filled" on:click={() => openContactModal()}>Lets Talk!</button>
-        <a class="btn btn-lg variant-filled" href="Hugo_Korte_Resume_2023-25-1.pdf" download="Hugo_Korte_Resume_2023-25-1">Resume</a>
+        <button class="btn lg:btn-lg variant-filled-primary" on:click={() => openContactModal()}>Lets Talk!</button>
+        <a class="btn lg:btn-lg variant-filled-secondary" href="Hugo_Korte_Resume_2023-25-1.pdf" download="Hugo_Korte_Resume_2023-25-1">Resume</a>
     </div>
 </div>
 
