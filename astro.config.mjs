99f041c5428d6745b99a124320fb81c787157cde
hugos68 <hugokorteapple@gmail.com>
2024-04-16 14:53:20 +0200

    Removed astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), db()]
+  integrations: [mdx(), tailwind()]
 });
