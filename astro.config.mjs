9d8ac785d5d9415020db733fb9600e8b66636ed1
hugos68 <hugokorteapple@gmail.com>
2024-04-16 17:15:49 +0200

    Further progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+	integrations: [
+		mdx({
+			rehypePlugins: [
+				[rehypeAutolinkHeadings, { behavior: "wrap" }],
+				[
+					rehypeExternalLinks,
+					{
+						target: "_blank",
+						content: { type: "text", value: " 🡕" },
+					},
+				],
+			],
+		}),
+		tailwind(),
+	],
 });
